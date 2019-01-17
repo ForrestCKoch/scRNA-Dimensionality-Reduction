116fb68f9d0fcf70e47135c9d9a621ba8210359d
Forrest Koch <forrest.koch@unsw.edu.au>
2019-01-17 13:12:38 +1100

    make more user friendly

--- src/embedding_tests.py
+++ src/embedding_tests.py
@@ -7,62 +7,84 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-from sklearn.manifold import TSNE as sk_tsne
+from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
-from MulticoreTSNE import MulticoreTSNE as mc_tsne
 import umap
+
+import argparse
 
 NPROCS=10
 
-def get_embedding(embed_func,npoints):
-    ds = E18MouseData('GSE93421_brain_aggregate_matrix.hdf5',
-                      nproc=NPROCS,
-                      selection=list(range(0,npoints)))
-    print('Generating Embedding ...')
-    embedding = embed_func(ds.cells)
-    return embedding
-
-def get_embedding(embed_func,ds_path):
-    ds = DuoBenchmark(ds_path,log1p=True) 
+def get_embedding(embed_func,ds):
     print('Generating Embedding ...')
     embedding = embed_func(ds.data)
     return embedding
 
 def umap_to_tsne(x):
     semi_embedded = umap.UMAP(n_components=50).fit_transform(x)
-    embedded = mc_tsne(n_components=2,n_jobs=NPROCS).fit_transform(semi_embedded)
+    embedded = TNSE(n_components=2,n_jobs=NPROCS).fit_transform(semi_embedded)
     return embedded
 
 def pca_to_tsne(x):
     semi_embedded = PCA(n_components=50).fit_transform(x)
-    embedded = mc_tsne(n_components=2,n_jobs=NPROCS).fit_transform(semi_embedded)
+    embedded = TNSE(n_components=2,n_jobs=NPROCS).fit_transform(semi_embedded)
     return embedded
 
 if __name__ == '__main__':
 
-    embed_opt = sys.argv[1]
-    ds_opt = sys.argv[2]
-    ds_path = 'data/datasets/'+ds_opt+'.csv'
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--method",
+        choice = ["umap","pca","tsne"],
+        default = 'pca'
+    )
 
-    if embed_opt == 'umap':
-        embed_func = umap.UMAP(n_components=50).fit_transform
-    elif embed_opt == 'pca':
-        embed_func = PCA(n_components=50).fit_transform
-    elif embed_opt == 'umap-mctsne':
+    parser.add_argument(
+        "--dataset",
+        choice = ["mouse","koh","kumar",
+                  "simk4easy","simk4hard","simk8hard",
+                  "zhengmix4eq","zhengmix8eq"],
+        default = 'koh' 
+    )
+
+    parser.add_argument(
+        "--dims",
+        type=int,
+        default=50
+    )
+
+    parser.parse_args()
+
+    
+    if dataset = 'mouse':
+        ds = E18MouseData('GSE93421_brain_aggregate_matrix.hdf5',
+                          nproc=NPROCS,
+                          selection=list(range(0,npoints)))
+    else:
+        ds_path = 'data/datasets/'+dataset+'.csv'
+        ds = DuoBenchmark(ds_path,log1p=True) 
+
+    if method == 'umap':
+        embed_func = umap.UMAP(n_components=dims).fit_transform
+    elif method == 'pca':
+        embed_func = PCA(n_components=dims).fit_transform
+    elif method == 'tsne':
+        embed_func = TSNE(n_components=dims).fit_transform
+    elif method == 'umap-mctsne':
         embed_func = umap_to_tsne
-    elif embed_opt == 'pca-mctsne':
+    elif method == 'pca-mctsne':
         embed_func = pca_to_tsne
     else:
         print("ERROR: Invalid embedding option", file=sys.stderr)
         exit()
 
-    embedded = get_embedding(embed_func,ds_path)
+    embedded = get_embedding(embed_func,ds)
 
     #print('saving image')
     #plt.scatter(embedded[:,0],embedded[:,1])
-    #plt.savefig('data/plots/'+embed_opt+'-250k.pdf')
+    #plt.savefig('data/plots/'+method+'-250k.pdf')
 
     print('saving embedding')
-    with open('data/embeddings/'+ds_opt+'-'+embed_opt+'.pickle','wb') as fh:
+    with open('data/embeddings/'+dataset+'-'+method+'-'+dims+'.pickle','wb') as fh:
         pickle.dump(embedded,fh,protocol=4)
 
