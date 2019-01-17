69e1db0c0fc7c7e216289adee66db12d35895d30
Forrest Koch <forrest.koch@unsw.edu.au>
2019-01-17 11:29:44 +1100

    used normalized counts instead

--- src/get_datasets.R
+++ src/get_datasets.R
@@ -6,7 +6,7 @@ library(DuoClustering2018)
 if(!file.exists('data/datasets/zhengmix8eq.csv')){
     cat('Generating zhengmix8eq.csv\n')
     zheng8 <- sce_full_Zhengmix8eq()
-    df <- data.frame(counts(zheng8))
+    df <- data.frame(assays(zheng8)$normcounts)
     names(df) <- paste(names(df),'-',colData(zheng8)$phenoid)
     write.table(df,file='data/datasets/zhengmix8eq.csv',sep=',',row.names=FALSE)
 }
@@ -14,7 +14,7 @@ if(!file.exists('data/datasets/zhengmix8eq.csv')){
 if(!file.exists('data/datasets/zhengmix4eq.csv')){
     cat('Generating zhengmix4eq.csv\n')
     zheng4 <- sce_full_Zhengmix4eq()
-    df <- data.frame(counts(zheng4))
+    df <- data.frame(assays(zheng4)$normcounts)
     names(df) <- paste(names(df),'-',colData(zheng4)$phenoid)
     write.table(df,file='data/datasets/zhengmix4eq.csv',sep=',',row.names=FALSE)
 }
@@ -22,7 +22,7 @@ if(!file.exists('data/datasets/zhengmix4eq.csv')){
 if(!file.exists('data/datasets/simk4easy.csv')){
     cat('Generating simk4easy.csv\n')
     simk4easy <- sce_full_SimKumar4easy()
-    df <- data.frame(counts(simk4easy))
+    df <- data.frame(assays(simk4easy)$normcounts)
     names(df)<-paste(simk4easy$Cell,'-',simk4easy$Group)
     write.table(df,file='data/datasets/simk4easy.csv',row.names=FALSE,sep=',')
 }
@@ -30,7 +30,7 @@ if(!file.exists('data/datasets/simk4easy.csv')){
 if(!file.exists('data/datasets/simk4hard.csv')){
     cat('Generating simk4hard.csv\n')
     simk4hard <- sce_full_SimKumar4hard()
-    df <- data.frame(counts(simk4hard))
+    df <- data.frame(assays(simk4hard)$normcounts)
     names(df)<-paste(simk4hard$Cell,'-',simk4hard$Group)
     write.table(df,file='data/datasets/simk4hard.csv',row.names=FALSE,sep=',')
 }
@@ -38,7 +38,7 @@ if(!file.exists('data/datasets/simk4hard.csv')){
 if(!file.exists('data/datasets/simk8hard.csv')){
     cat('Generating simk8hard.csv\n')
     simk8hard <- sce_full_SimKumar8hard()
-    df <- data.frame(counts(simk8hard))
+    df <- data.frame(assays(simk8hard)$normcounts)
     names(df)<-paste(simk8hard$Cell,'-',simk8hard$Group)
     write.table(df,file='data/datasets/simk8hard.csv',row.names=FALSE,sep=',')
 }
@@ -46,7 +46,7 @@ if(!file.exists('data/datasets/simk8hard.csv')){
 if(!file.exists('data/datasets/koh.csv')){
     cat('Generating koh.csv\n')
     koh <- sce_full_Koh()
-    df <- data.frame(counts(koh))
+    df <- data.frame(assays(koh)$normcounts)
     names(df)<-paste(names(df),'-',colData(koh)$phenoid)
     write.table(df,file='data/datasets/koh.csv',row.names=FALSE,sep=',')
 }
@@ -54,7 +54,7 @@ if(!file.exists('data/datasets/koh.csv')){
 if(!file.exists('data/datasets/kumar.csv')){
     cat('Generating kumar.csv\n')
     kumar <- sce_full_Kumar()
-    df <- data.frame(counts(kumar))
+    df <- data.frame(assays(kumar)$normcounts)
     names(df)<-paste(names(df),'-',colData(kumar)$phenoid)
     write.table(df,file='data/datasets/kumar.csv',row.names=FALSE,sep=',')
 }
