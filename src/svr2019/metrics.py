c969f9cb0dacb50f2c209793c121420c10a5bad0
Forrest Koch <forrest.koch@unsw.edu.au>
2019-01-22 17:04:01 +1100

    implement dunn index

--- src/svr2019/metrics.py
+++ src/svr2019/metrics.py
@@ -5,6 +5,8 @@ import numpy as np
 from scipy.spatial.distance import pdist, euclidean
 from sklearn.utils import safe_indexing
 
+import sklearn.metrics as metrics
+
 def davies_bouldin_score(X, labels):
     """
     Taken from:
@@ -34,7 +36,7 @@ def davies_bouldin_score(X, labels):
 
     return np.mean(scores)
 
-def dunn_index(points, labels, metric = 'euclidean'):
+def dunn_index(X, labels, metric = 'euclidean'):
     """
     Calculate the Dunn Index for the provided clustering
 
@@ -47,8 +49,23 @@ def dunn_index(points, labels, metric = 'euclidean'):
     # TODO: implement I am currently concerned about
     #       runtime for larger datasets, so I am
     #       leaving this until later ...    
+    n_labels = max(labels)+1
 
-    return None
+    intra_dists = np.zeros(n_labels)
+    centroids = np.zeros((n_labels, len(X[0])), dtype=np.float)
+    for k in range(n_labels):
+        cluster_k = safe_indexing(X, labels == k)
+        centroid = cluster_k.mean(axis=0)
+        centroids[k] = centroid
+        intra_dists[k] = np.average(metrics.pairwise_distances(
+            cluster_k, [centroid]))
+
+    centroid_distances = metrics.pairwise_distances(centroids)
+    for i in range(0,len(centroid_distances)):
+        centroid_distances[i][i] = np.inf
+    
+    return min([min(x) for x in centroid_distances])/max(intra_dists)
+    #return min([min(x) for x in centroid_distances]),max(intra_dists)
 
 
 if __name__ == '__main__':
