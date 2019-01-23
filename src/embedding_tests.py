3609a73f5cc4aefd9ba043012c6c52781f3b704f
Forrest Koch <forrest.koch@unsw.edu.au>
2019-01-23 12:20:52 +1100

    restructure and add new options

--- src/embedding_tests.py
+++ src/embedding_tests.py
@@ -2,31 +2,36 @@ from svr2019.datasets import *
 
 import pickle
 import sys
+import os
 
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE, Isomap, LocallyLinearEmbedding,
+                             SpectralEmbedding, MDS
+from sklearn.decomposition import PCA, FactorAnalysis, FastICA
 import umap
+from MulticoreTSNE import MulticoreTSNE as MCTSNE
 
 import argparse
 
-def get_embedding(embed_obj,data):
+def get_embedding(model,data):
     print('Generating Embedding ...')
-    embedding = embed_obj.fit_transform(data)
+    embedding = model.fit_transform(data)
     return embedding
 
 def str2bool(value):
     return value.lower() == 'true'
 
-if __name__ == '__main__':
-
+def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--method",
-        choices = ["umap","pca","tsne"],
+        choices = ["umap","pca","tsne",
+                   "mctsne", "isomap", "lle",
+                   "spectral", "mds",
+                   "fa","fica"],
         default = 'pca'
     )
 
@@ -57,6 +62,12 @@ if __name__ == '__main__':
     )
 
     parser.add_argument(
+        "--njobs",
+        type=int,
+        default=1
+    )
+
+    parser.add_argument(
         "--log1p",
         type=str2bool,
         default=False
@@ -67,10 +78,55 @@ if __name__ == '__main__':
         type=str2bool,
         default=False
     )
-
-    args = parser.parse_args()
-
     
+    return parser
+
+def get_model(args):
+    if args.method == 'umap':
+        model = umap.UMAP(n_components=args.dims)
+    elif args.method == 'pca':
+        model = PCA(n_components=args.dims)
+    elif agrs.method == 'tsne':
+        model = TSNE(n_components=args.dims)
+    elif agrs.method == 'mctsne':
+        model = MCTSNE(n_components=args.dims,n_jobs=args.njobs)
+    elif agrs.method == 'spectral':
+        model = SpectralEmbedding(n_components=args.dims,n_jobs=args.njobs)
+    elif agrs.method == 'lle':
+        model = LocallyLinearEmbedding(n_components=args.dims,n_jobs=args.njobs)
+    elif agrs.method == 'isomap':
+        model = Isomap(n_components=args.dims,n_jobs=args.njobs)
+    elif agrs.method == 'mds':
+        model = MDS(n_components=args.dims,n_jobs=args.njobs)
+    elif agrs.method == 'fa':
+        model = FactorAnalysis(n_components=args.dims)
+    elif agrs.method == 'fica':
+        model = FastICA(n_components=args.dims)
+    else:
+        print("ERROR: Invalid embedding option", file=sys.stderr)
+        exit()
+    return model
+
+def write_results(model,embedded,args):
+    # make our directories if we have to
+    embed_dir = os.path.join('data','embeddings',args.dataset,args.method)
+    model_dir = os.path.join('data','model',args.dataset,args.method)
+    if not os.path.exists(embed_dir):
+        os.makedirs(embed_dir)
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+    print('saving embedding')
+    log_flag = str(args.log1p or args.log_trans)
+    filename  = str(args.dims)+'-log-'+log_flag+'.pickle'
+
+    with open(os.path.join(embed_dir,filename),'wb') as fh:
+        pickle.dump(embedded,fh,protocol=4)
+
+    with open(os.path.join(model_dir,filename),'wb') as fh:
+        pickle.dump(model,fh,protocol=4)
+
+def get_data(args):
     if args.dataset == 'mouse':
         ds_path = 'data/datasets/GSE93421_brain_aggregate_matrix.hdf5'
         data = E18MouseData(ds_path,
@@ -80,30 +136,13 @@ if __name__ == '__main__':
     else:
         ds_path = 'data/datasets/'+args.dataset+'.csv'
         data = DuoBenchmark(ds_path,log_trans=args.log_trans,log1p=args.log1p).data
+    return data
 
-    if args.method == 'umap':
-        embed_obj = umap.UMAP(n_components=args.dims)
-    elif args.method == 'pca':
-        embed_obj = PCA(n_components=args.dims)
-    elif agrs.method == 'tsne':
-        embed_obj = TSNE(n_components=args.dims)
-    else:
-        print("ERROR: Invalid embedding option", file=sys.stderr)
-        exit()
+if __name__ == '__main__':
 
-    embedded = get_embedding(embed_obj,data)
-
-    #print('saving image')
-    #plt.scatter(embedded[:,0],embedded[:,1])
-    #plt.savefig('data/plots/'+method+'-250k.pdf')
-
-    print('saving embedding')
-    emb_file='data/embeddings/'+args.dataset+'-'+args.method+'-'+str(args.dims)+'-log1p-'+str(args.log1p)+'.pickle'
-    obj_file='data/embeddings/'+args.dataset+'-'+args.method+'-'+str(args.dims)+'-log1p-'+str(args.log1p)+'-object.pickle'
-
-    with open(emb_file,'wb') as fh:
-        pickle.dump(embedded,fh,protocol=4)
-
-    with open(obj_file,'wb') as fh:
-        pickle.dump(embed_obj,fh,protocol=4)
-
+    parser = get_parser()
+    args = parser.parse_args()
+    data = get_data(args)
+    model = get_model(args)
+    embedded = get_embedding(model,data)
+    write_results(model,embedded,args)
