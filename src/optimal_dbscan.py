2c2f05075c38a507f137ee8eaca2cdc066c44802
Forrest <forrest.c.koch@gmail.com>
2019-05-09 11:56:49 +1000

    refactor optimal dbscan to be more flexible

--- src/optimal_dbscan.py
+++ src/optimal_dbscan.py
@@ -6,6 +6,7 @@
 import pickle
 import sys
 import os
+import itertools
 
 import numpy as np
 
@@ -18,6 +19,7 @@ from sklearn.metrics import normalized_mutual_info_score
 from sklearn.preprocessing import LabelEncoder
 
 from svr2019.datasets import *
+from svr2019.metrics import davies_bouldin_score
 
 def warn(*args, **kwargs):
     """
@@ -25,47 +27,116 @@ def warn(*args, **kwargs):
     """
     pass
 
+def dbscan_trial(data,true_labels,eps,min_samp):
+    """
+    Perfoms DBSCAN with the given parameters and returns:
+        - number of clusters
+        - variance ratio criterion
+        - silhouette score
+        - davies bouldin index
+        - adjusted rand index
+        - normalized mutual information
+
+    :param data: data to be clustered
+    :param true_labels: true labels for the data (for ari & nmi)
+    :param eps: choice of epsilon for DBSCAN
+    :param min_samp: choice of min_samples for DBSCAN
+    :return: dictionary in form:
+        {'clusters','epsilon','min_samples','vrc','ss','db','ari','nmi'}
+    """
+
+    labels = DBSCAN(eps=eps,min_samples=min_samp).fit(data).labels_
+    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
+    if n_clusters > 1:
+        # TODO: add Davies Bouldin and Dunn Index
+        vrc = calinski_harabaz_score(emb,labels)
+        ss = silhouette_score(emb,labels)
+        db = davies_bouldin_score(emb,labels)
+        ari = adjusted_rand_score(true_labels,labels)
+        nmi = normalized_mutual_info_score(true_labels,labels)
+    else:
+        vrc,ss,db,ari,nmi = [np.nan]*5
+    return {'clusters':n_clusters,
+            'epsilon':eps,
+            'min_samples':min_samp,
+            'vrc':vrc,
+            'ss':ss,
+            'db':db,
+            'ari':ari,
+            'nmi':nmi}
+
+
+def dbscan_optimization(data,true_labels,eps_choices,ms_choices):
+    """
+    Performs brute force grid search optimization for each of our
+    considered metrics.  It returns a dictionary where keys are
+    the metrics (vrc, nmi, ari, ...), and each entry is 
+    the return for the optimal result of `dbscan_trial` for that metric.
+
+    :param data: data to perform optimization on
+    :param true_labels: true labels for the data
+    :param eps_choices: list of epsilon choices for grid search
+    :param ms_choices: list of minimum sample choices for grid search
+    :return: dictionary in form {'vrc','ss','db','ari','nmi'}
+    """
+    def isBetter(x,y,m):
+        if not y[m]:
+            return True
+        if m == 'db': # special case for davies bouldin score
+            return x[m]<y[m][m]
+        else:
+            return x[m]>y[m][m]
+    
+    optimal_results = {'vrc':False,
+                       'ss':False,
+                       'db':False,
+                       'ari':False,
+                       'nmi':False}
+    for eps,ms in itertools.product(eps_choices,ms_choices):
+        outcome = dbscan_trial(data,true_labels,eps,ms)
+        if outcome['clusters'] < 2:
+            continue
+        for metric in optimal_results.keys():
+            if isBetter(outcome,optimal_results,metric):
+                optimal_results[metric] = outcome 
+    return optimal_results 
+
+def print_optimal_dbscans(ds,m,f,d):
+    """
+    Print out the results of `optimize_dbscan`
+
+    :param ds: name of dataset
+    :param m: name of method
+    :param f: name of pickle file
+    :param d: results dictionary
+    """
+    for key in d.keys():
+        print(','.join([ds,m,f,key]+[str(d[key][x]) for x in d[key].keys()]))
+
 if __name__ == '__main__':
     import warnings
     warnings.warn = warn
 
-    EPS_CHOICES=[0.001,0.0025,0.005,0.0075,0.01,0.025,0.05,0.075,0.1,0.25,0.5,0.75,1.0,2.5,5.0]
-    MIN_SAMP_CHOICES=[1,2,3,4,5,10,15,20,25,35,50]
-
     dset = sys.argv[1]
-    #emb_file = 'data/embeddings/' + dset + '/umap/4-log-False.pickle'
-    #emb_file = sys.argv[2]
-    #emb = pickle.load(open(emb_file,'rb'))
-    #raw_data = DuoBenchmark('data/datasets/'+dset+'.csv',split_head=False)
-    dataset_file = 'data/datasets/'+dset+'.csv'
+    method = sys.argv[2]
+    # This method of extracting labels won't work for older datasets
+    dataset_file = 'data/headers/'+dset+'.csv'
     with open(dataset_file,'r') as fh:
         cell_types = fh.readline().rstrip('\n').split(',')
 
     le = LabelEncoder()
-    le.fit(cell_types)
-    true_labels = le.transform(cell_types)
+    true_labels = le.fit_transform(cell_types)
+
+    # Extract following code into function which takes:
+    # - true labels
+    # - pickle file to load 
+
+    eps_choices = list(np.arange(0.025,0.5,0.025))
+    ms_choices = list(range(2,15))
+    for emb_file in os.listdir('data/embeddings/'+dset+'/'+method):
+        full_path = 'data/embeddings/'+dset+'/'+method+'/'+emb_file
+        emb = pickle.load(open(full_path,'rb'))
+        result = dbscan_optimization(emb,true_labels,eps_choices,ms_choices)
+        print_optimal_dbscans(dset,method,emb_file,result)
 
 
-    for emb_file in os.listdir('data/embeddings/'+dset+'/lda'):
-        emb_file = 'data/embeddings/'+dset+'/lda/'+emb_file
-        emb = pickle.load(open(emb_file,'rb'))
-
-        for eps in EPS_CHOICES:
-            for min_samp in MIN_SAMP_CHOICES:
-                db = DBSCAN(eps=eps,min_samples=min_samp).fit(emb)
-                labels = db.labels_
-                n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
-                if n_clusters > 1:
-                    vrc = calinski_harabaz_score(emb,labels)
-                    ss = silhouette_score(emb,labels)
-                    ari = adjusted_rand_score(true_labels,labels)
-                    nmi = normalized_mutual_info_score(true_labels,labels)
-                else:
-                    vrc = -1
-                    ss = -1
-                    ari = -1
-                    nmi = -1
-
-                line = [eps,min_samp,n_clusters,vrc,ss,ari,nmi] 
-                print(','.join([emb_file]+[str(x) for x in line]))
-                sys.stdout.flush()
