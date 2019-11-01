3f6946d815bd9d14f75959f9698142767aefdc31
Forrest Koch <forrest.koch@unsw.edu.au>
2019-11-01 16:53:37 +1100

    add support for new methods

--- src/generate_embedding.py
+++ src/generate_embedding.py
@@ -10,14 +10,23 @@ from sc_dr.datasets import *
 import pickle
 import sys
 import os
+import argparse
+import time
+
+
+import numpy as np
 
 from sklearn.manifold import TSNE, Isomap, LocallyLinearEmbedding,\
                              SpectralEmbedding, MDS
 from sklearn.decomposition import PCA, FactorAnalysis, FastICA,\
-                                  LatentDirichletAllocation, NMF
+                                  LatentDirichletAllocation, NMF,\
+                                  KernelPCA, IncrementalPCA, SparsePCA,\
+                                  MiniBatchSparsePCA, TruncatedSVD
 from sklearn.preprocessing import scale
+from sklearn.random_projection import GaussianRandomProjection, SparseRandomProjection
 
-import time
+import nimfa
+from ivis import Ivis
 
 import pandas as pd
 
@@ -30,16 +39,16 @@ except ImportError:
 from MulticoreTSNE import MulticoreTSNE as MCTSNE
 
 try:
+    from saucie import SAUCIE, Loader
+    SAUCIE_AVAILABLE=True
+except ImportError:
+    SAUCIE_AVAILABLE=False
+
+try:
     import scscope
     SCSCOPE_AVAILABLE=True
 except ImportError:
     SCSCOPE_AVAILABLE=False
-
-try:
-    from ZIFA import block_ZIFA
-    ZIFA_AVAILABLE=True
-except ImportError:
-    ZIFA_AVAILABLE=False
 
 try:
     from phate import PHATE
@@ -47,7 +56,30 @@ try:
 except ImportError:
     PHATE_AVAILABLE=False
 
-import argparse
+try:
+    from tga import TGA
+    TGA_AVAILABLE=True
+except ImportError:
+    TGA_AVAILABLE=False
+
+try:
+    from vpac import VPAC
+    VPAC_AVAILABLE=True
+except ImportError:
+    VPAC_AVAILABLE=False
+
+try:
+    from vasc import vasc
+    VASC_AVAILABLE=True
+except ImportError:
+    VASC_AVAILABLE=False
+
+try:
+    from ZIFA import block_ZIFA
+    ZIFA_AVAILABLE=True
+except ImportError:
+    ZIFA_AVAILABLE=False
+
 
 class ZIFA_Wrapper():
 
@@ -97,6 +129,116 @@ class ScScope():
         embedding,_,_ = scscope.predict(data,self.model,batch_effect=[])
         return embedding
 
+class NimfaWrapper():
+    """
+    Wrapper calss for nimfa functions to be applied like sklearn functions
+    """
+
+    def __init__(self,func,k):
+        self.k = k
+        self.func = func
+
+    def fit_transform(self,data):
+        self.model = self.func(np.transpose(data),rank=self.k)
+        self.model_fit = self.model()
+        return self.model_fit.coef().T
+
+class SaucieWrapper():
+    def __init__(self,k):
+        self.k = k
+        pass
+
+    def fit_transform(self,data):
+        self.model = SAUCIE(data.shape[1],layers=[512,256,128,self.k])
+        loadtrain = Loader(data,shuffle=True)
+        # Keep training until loss stabilizes
+        losses = list()
+        max_epochs=500
+        not_converged = True
+        while(not_converged):
+            self.model.train(loadtrain,steps=int(np.floor(data.shape[0]/256)))
+            losses.append(float(self.model.get_loss(loadtrain)))
+            if len(losses) < 10: # keep training
+                continue
+            if loadtrain.epoch >= max_epochs: # give up
+                not_converged = False
+            
+            mloss = np.mean(losses[-10:-5])
+            nloss = np.mean(losses[-5:])
+            perc_change = np.abs((nloss-mloss)/mloss)
+            if perc_change < 0.001:
+                not_converged = False
+
+        if(loadtrain.epoch >= max_epochs):
+            print("Failed to converge in {} epochs".format(max_epochs))
+        else:
+            print("Finished training saucie model in {} epochs".format(loadtrain.epoch))
+        return self.model.get_embedding(loadtrain)
+
+class VascWrapper():
+    def __init__(self,k):
+        self.k
+
+    def fit_transform(self,data):
+        return vasc(data,epoch=1000,latent=self.k)
+
+class VpacWrapper():
+    def __init__(self,k):
+        self.k = k
+
+    def fit_transform(self,data):
+        self.model = VPAC(np.transpose(data),latent_dim=self.k,n_components=self.k)
+        self.model.fit()
+        return np.transpose(self.model.transform(np.transpose(data)))
+    
+def _embedding_error(*args):
+    print("ERROR: invalid emedding method",file=sys.stderr)
+    exit()
+
+model_dict = {
+    'bd': lambda args: NimfaWrapper(nimfa.Bd,args.dims),
+    'fa':lambda args: FactorAnalysis(n_components=args.dims),
+    'fica':lambda args: FastICA(n_components=args.dims),
+    'grp':lambda args: GaussianRandomProjection(n_components=args.dims),
+    'icm': lambda args: NimfaWrapper(nimfa.Icm,args.dims),
+    'ipca':lambda args: IncrementalPCA(n_components=args.dims), 
+    'ivis':lambda args: Ivis(embedding_dims=args.dims, k=8),
+    'isomap':lambda args: Isomap(n_components=args.dims,n_jobs=args.njobs),
+    'kpca-pol':lambda args: KernelPCA(n_components=args.dims, kernel='poly', n_jobs=args.njobs),
+    'kpca-rbf':lambda args: KernelPCA(n_components=args.dims, kernel='rbf', n_jobs=args.njobs),
+    'kpca-sig':lambda args: KernelPCA(n_components=args.dims, kernel='sigmoid', n_jobs=args.njobs),
+    'kpca-cos':lambda args: KernelPCA(n_components=args.dims, kernel='cosine', n_jobs=args.njobs),
+    'lda':lambda args: LatentDirichletAllocation(args.dims),
+    'lle':lambda args: LocallyLinearEmbedding(n_components=args.dims,n_jobs=args.njobs),
+    'lfnmf': lambda args: NimfaWrapper(nimfa.Lfnmf,args.dims),
+    'lsnmf': lambda args: NimfaWrapper(nimfa.Lsnmf,args.dims),
+    'mctsne':lambda args: MCTSNE(n_components=args.dims,n_jobs=args.njobs),
+    'mds':lambda args: MDS(n_components=args.dims,n_jobs=args.njobs),
+    'nmf':lambda args: NMF(args.dims),
+    'nmf2': lambda args: NimfaWrapper(nimfa.Nmf,args.dims),
+    'nsnmf': lambda args: NimfaWrapper(nimfa.Nsnmf,args.dims),
+    'pca':lambda args: PCA(n_components=args.dims),
+    'phate':lambda args: PHATE(n_components=args.dims,n_jobs=args.njobs) if PHATE_AVAILABLE else _embedding_error(),
+    'pmf': lambda args: NimfaWrapper(nimfa.Pmf,args.dims),
+    'psmf': lambda args: NimfaWrapper(nimfa.Psmf,args.dims),
+    'saucie': lambda args: SaucieWrapper(args.dims) if SAUCIE_AVAILABLE else _embedding_error(),
+    'scscope':lambda args: ScScope(args.dims) if SCSCOPE_AVAILABLE else _embedding_error,
+    'sepnmf': lambda args: NimfaWrapper(nimfa.SepNMF,args.dims),
+    'spca':lambda args: SparsePCA(n_components=args.dims, n_jobs=args.njobs, normalize_components=True),
+    'spca-batch': lambda args: MiniBatchSparsePCA(n_components=args.dims, n_jobs=args.njobs, normalize_components=True),
+    'spectral':lambda args: SpectralEmbedding(n_components=args.dims,n_jobs=args.njobs),
+    'snmf': lambda args: NimfaWrapper(nimfa.Snmf,args.dims),
+    'srp': lambda args: SparseRandomProjection(n_components=args.dims),
+    'tga': lambda args: TGA(n_components=args.dims) if TGA_AVAILABLE else _embedding_error(),
+    'tsvd': lambda args: TruncatedSVD(n_components=args.dims),
+    'tsne':lambda args: TSNE(n_components=args.dims),
+    'umap':lambda args: umap.UMAP(n_components=args.dims) if UMAP_AVAILABLE else _embedding_error(),
+    'vpac':lambda args: VpacWrapper(args.dims) if VPAC_AVAILABLE else _embedding_error(),
+    'vasc':lambda args: VascWrapper(args.dims) if VPAC_AVAILABLE else _embedding_error(),
+    'zifa':lambda args: ZIFA_Wrapper(args.dims) if ZIFA_AVAILABLE else _embedding_error(),
+}
+
+
 def get_embedding(model,data,to_scale=False):
     """
     Wrapper function around the `fit_transform` function
@@ -122,11 +264,7 @@ def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--method",
-        choices = ["umap","pca","pca-scaled","tsne",
-                   "mctsne", "isomap", "lle",
-                   "nmf","lda","zifa",
-                   "spectral", "mds","phate",
-                   "fa","fica","scscope","rpca"],
+        choices = list(model_dict.keys()),
         default = 'pca',
         help="method for dimension reduction"
     )
@@ -203,6 +341,7 @@ def get_parser():
 
     return parser
 
+
 def get_model(args):
     """
     Get the base model class for the dimension reduction
@@ -211,44 +350,11 @@ def get_model(args):
     :param args: output of ArgumentParser().parse_args()
     :return: model to be used for dimension reduction
     """
-    if args.method == 'umap' and UMAP_AVAILABLE:
-        model = umap.UMAP(n_components=args.dims)
-    elif args.method == 'pca':
-        model = PCA(n_components=args.dims)
-    elif args.method == 'pca-scaled':
-        model = ScaledPCA(args.dims)
-    elif args.method == 'rpca':
-        model = RandomizedPCA(n_components=args.dims)
-    elif args.method == 'tsne':
-        model = TSNE(n_components=args.dims)
-    elif args.method == 'mctsne':
-        model = MCTSNE(n_components=args.dims,n_jobs=args.njobs)
-    elif args.method == 'spectral':
-        model = SpectralEmbedding(n_components=args.dims,n_jobs=args.njobs)
-    elif args.method == 'lle':
-        model = LocallyLinearEmbedding(n_components=args.dims,n_jobs=args.njobs)
-    elif args.method == 'isomap':
-        model = Isomap(n_components=args.dims,n_jobs=args.njobs)
-    elif args.method == 'mds':
-        model = MDS(n_components=args.dims,n_jobs=args.njobs)
-    elif args.method == 'fa':
-        model = FactorAnalysis(n_components=args.dims)
-    elif args.method == 'fica':
-        model = FastICA(n_components=args.dims)
-    elif args.method == 'zifa' and ZIFA_AVAILABLE:
-        model = ZIFA_Wrapper(args.dims)
-    elif args.method == 'lda':
-        model = LatentDirichletAllocation(args.dims)
-    elif args.method == 'nmf':
-        model = NMF(args.dims)
-    elif args.method == 'scscope' and SCSCOPE_AVAILABLE:
-        model = ScScope(args.dims)
-    elif args.method == 'phate' and PHATE_AVAILABLE:
-        model = PHATE(args.dims)
-    else:
-        print("ERROR: Invalid embedding option", file=sys.stderr)
-        exit()
-    return model
+
+    if not args.method in model_dict:
+        _embedding_error()
+
+    return model_dict[args.method](args)
 
 def write_results(embedded,labels,args):
     """
