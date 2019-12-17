a0bc95aa22c20393bcf9a792b23c422bcc9bd60a
Forrest Koch <forrest.koch@unsw.edu.au>
2019-12-17 22:07:17 +1100

    update trial scripts

---
+++ scripts/run_trial.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+queuefile=$1
+dims=$(echo $1|rev|cut -d'/' -f1|rev|cut -d'.' -f1)
+method=$(echo $1|cut -d'/' -f5)
+metric=$(echo $1|cut -d'/' -f6)
+count=$(echo $1|cut -d'/' -f4)
+dataset=$(echo $1|cut -d'/' -f3)
+
+pkl="data/embeddings/$dataset/$count/$method/${dims}.pkl"
+logdir=data/results/dbscan/${dataset}/$count/$method/$metric
+mkdir -p $logdir
+
+echo $queuefile
+python3 src/run_dbscan_trials.py $pkl $queuefile > $logdir/${dims}.csv
