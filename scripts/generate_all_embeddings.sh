cacdcc6927bd0d4fec9e054dc11c894f1257758e
Forrest Koch <forrest.koch@unsw.edu.au>
2020-01-29 16:03:47 +1100

    saving scripts (i.e generate_queues.sh)

--- scripts/generate_all_embeddings.sh
+++ scripts/generate_all_embeddings.sh
@@ -6,16 +6,17 @@
 ###############################################################################
 
 #for m in pca umap mctsne lda lle isomap fa fica nmf spectral zifa; do  # methods
-for m in phate; do  # methods
+for m in ivis; do  # methods
 #for m in zifa; do 
-for d in $(ls data/datasets/pddf|cut -d'.' -f1); do # datasets
+for d in $(ls data/datasets/pddf|cut -d'.' -f1|egrep -v '(Lin|macosko)'); do # datasets
 for e in 2 4 8 16 32 48 96; do # dimensions
     ds=$(echo $d|cut -d'_' -f2-) # extract dataset name from file
     c=$(echo $d|cut -d'_' -f1)   # extract count type (normcounts, logcounts, etc ...) 
     o="data/embeddings/$ds/$c/$m" # output directory
     mkdir -p $o/logs
     logfile="$o/logs/${e}.log"
-    qsub -cwd -pe mpi 4 -q long.q -N run -o $logfile -j y -v dataset=$d,method=$m,dims=$e,name=$e,outdir=$o,njobs=4 scripts/run_generate_embedding.sh
+    #qsub -cwd -pe mpi 4 -q long.q -N run -o $logfile -j y -v dataset=$d,method=$m,dims=$e,name=$e,outdir=$o,njobs=4 scripts/run_generate_embedding.sh
+    qsub -p 1024 -cwd -pe mpi 6 -binding linear:4 -q tmp.q -N run -o $logfile -j y -v dataset=$d,method=$m,dims=$e,name=$e,outdir=$o,njobs=4 scripts/run_generate_embedding.sh
     #qsub -cwd -pe mpi 8 -q bigmem.q -N run -o $logfile -j y -v dataset=$d,method=$m,dims=$e,name=$e,outdir=$o,njobs=8 scripts/run_generate_embedding.sh
 done
 done
