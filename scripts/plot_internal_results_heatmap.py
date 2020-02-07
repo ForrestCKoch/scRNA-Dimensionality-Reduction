0bdcd0f3f6e7247ee413a75ea85a31955c9ba5ec
ForrestCKoch <forrest.c.koch@gmail.com>
2020-02-07 14:38:01 +1100

    make some adjustments to IVM heatmap

--- scripts/plot_internal_results_heatmap.py
+++ scripts/plot_internal_results_heatmap.py
@@ -9,6 +9,11 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     x = pd.read_csv(sys.argv[1]).dropna()
+    #x = x[x['method'] != 'saucie']
+    y = pd.read_csv('data/results/dataset_info.csv')
+    # alter the following line to change the dataset ordering
+    x['dataset'] = pd.Categorical(x['dataset'],y.sort_values('protocol',ascending=False)['dataset'].values) 
+    #x['dataset'] = pd.Categorical(x['dataset'],y.sort_values('n_classes',ascending=False)['dataset'].values) 
     grouped = x.groupby(['dataset','method'])
 
     # Used to isolate "best" measures
@@ -18,7 +23,9 @@ if __name__ == '__main__':
     # Used to transform the "best" measures, so they can be sensibly scaled
     fdict = {'ss_euc':lambda x : x,'ss_seu':lambda x : x,'ss_cor':lambda x : x,'ss_cos':lambda x : x,'vrc':lambda x: np.log(x),'dbs':lambda x: 1/x if x > 0 else np.nan}
     trans = aggrd.transform(fdict) # apply transform
-    trans_mins = trans.groupby('dataset').agg(np.nanmin) # calculate minimums by group
+
+    # NOTE: the minimum is calculated as the 2nd worst performer in order to improve contrast
+    trans_mins = trans.groupby('dataset').agg(lambda z: np.partition(z,2,axis=None)[1]) # calculate minimums by group
     trans_maxs = trans.groupby('dataset').agg(np.nanmax) # calculate maximums by group
     scaled = (trans - trans_mins) / (trans_maxs - trans_mins) # apply the scaling
     
@@ -40,7 +47,7 @@ if __name__ == '__main__':
 
     xlabs = list(X.columns.levels[1])
 
-    ax = sns.heatmap(X,cmap='viridis',xticklabels=xlabs,yticklabels=ylabs)
+    ax = sns.heatmap(X,0,1,cmap='viridis',xticklabels=xlabs,yticklabels=ylabs)
     ax.hlines(sep_lines, colors='r', linestyles='dotted', *ax.get_xlim())
     plt.show()
 
