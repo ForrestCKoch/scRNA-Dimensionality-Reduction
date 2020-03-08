d75b79b85ddfd823a08e7772daea7b15b8e5b57a
ForrestCKoch <forrest.c.koch@gmail.com>
2020-03-08 17:34:52 +1100

    working on IVM analysis'

---
+++ scripts/ivm_analysis.R
@@ -0,0 +1,26 @@
+library('synchrony')
+x <- read.csv('data/results/internal_measures_standardized.csv')
+measures <- unique(x$measure)
+# Get the average by column
+X <- colMeans(subset(x,measure==measures[1])[,-c(1,2)],na.rm=T)
+for(i in measures[-c(1)]){
+    X<-rbind(X,colMeans(subset(x,measure==i)[,-c(1,2)],na.rm=T))
+}
+# to regenerate data/results/internal_measures_standardized_grouped_means.csv
+# uncomment the following lines
+# and calculate the total mean for the last column
+#y<-data.frame(X)
+#y$measure<-measure
+#write.csv(y,'data/results/internal_measures_standardized_grouped_means.csv')
+
+# Measure aggrement of different IVMs
+kw<-kendall.w(t(X),nrands=10000)
+print(kw)
+
+# My results were:
+# Kendall's W p-value (one-tailed test [greater]): 0.000999> kendall.w(t(X),nrands=10000)
+#  |======================================================================| 100%
+# Kendall's W (uncorrected for ties): 0.8708
+# Kendall's W (corrected for ties): 0.8708
+# Spearman's ranked correlation: 0.845
+
