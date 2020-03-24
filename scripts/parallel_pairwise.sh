d1ea52a046b641c4d058f8818f22494c3ab117db
Forrest Koch <forrest.koch@unsw.edu.au>
2020-03-24 15:44:48 +1100

    add some parallel processing scripts

---
+++ scripts/parallel_pairwise.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+dataset=$(echo $1|cut -d'.' -f1)
+counts=$(echo $1|cut -d'.' -f3)
+metric=$(echo $1|cut -d'.' -f2)
+echo $1
+python3.6 scripts/measure_pairwise_distances.py $metric $counts $dataset
