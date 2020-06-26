fix sign test pvalue mistake and add dbscan sign tests+plots

scripts/ivm_sign_test_pval_heatmaps_combined.py
@@ -12,7 +12,7 @@ def get_ranks():
     return pd.read_csv('writeup/spreadsheets/ivm_ranks.csv')
 
 def get_pvals(measure):
-    return pd.read_csv('writeup/spreadsheets/ivm_sign_tests/ivm_sign_test_'+measure+'.csv').set_index('X')
+    return pd.read_csv('writeup/spreadsheets/ivm_sign_tests/ivm_sign_test_'+measure+'.csv').set_index('Unnamed: 0')
 
 def fill_matrix(x):
     return x.fillna(0).values+x.fillna(0).T.values + np.eye(x.values.shape[0]) 
@@ -29,8 +29,8 @@ if __name__ == '__main__':
     ranks = get_ranks()
     for measure, ax in zip(['vrc','dbs','ss-euc','ss-seu','ss-cor','ss-cos'],axes.flatten()):
         pvals = get_pvals(measure)
-        pval_mat = fill_matrix(pvals)
-        ordered = sort_by_ranks(ranks,pval_mat,measure)
+        #pval_mat = fill_matrix(pvals)
+        ordered = sort_by_ranks(ranks,pvals.values,measure)
         sns.heatmap(ordered,cmap='viridis',xticklabels=ordered.index.tolist(),yticklabels=ordered.index.tolist(),cbar=False,ax=ax,vmin=0,vmax=0.5)
         ax.set_title(measure)
     plt.tight_layout()