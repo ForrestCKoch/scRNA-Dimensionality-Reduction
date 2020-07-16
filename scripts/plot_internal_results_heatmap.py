25d4ea91d71b612f3a6c57a044d7dc23cbd548a8
Forrest Koch <forrest.c.koch@gmail.com>
2020-07-16 21:06:09 +1000

    merging

--- scripts/plot_internal_results_heatmap.py
+++ scripts/plot_internal_results_heatmap.py
@@ -17,31 +17,25 @@ if __name__ == '__main__':
     grouped = x.groupby(['dataset','method'])
 
     # Used to isolate "best" measures
-    print('#')
     mdict = {'ss_euc':'max','ss_seu':'max','ss_cor':'max','ss_cos':'max','vrc':'max','dbs':'min'}
     aggrd = grouped.agg(mdict)
-    print('#')
 
     # Used to transform the "best" measures, so they can be sensibly scaled
     fdict = {'ss_euc':lambda x : x,'ss_seu':lambda x : x,'ss_cor':lambda x : x,'ss_cos':lambda x : x,'vrc':lambda x: np.log(x),'dbs':lambda x: 1/x if x > 0 else np.nan}
     trans = aggrd.transform(fdict) # apply transform
-    print('#')
 
     # NOTE: the minimum is calculated as the 2nd worst performer in order to improve contrast
     trans_mins = trans.groupby('dataset').agg(lambda z: np.partition(z,2,axis=None)[1]) # calculate minimums by group
     trans_maxs = trans.groupby('dataset').agg(np.nanmax) # calculate maximums by group
     scaled = (trans - trans_mins) / (trans_maxs - trans_mins) # apply the scaling
     #scaled = aggrd
-    print('#')
     
     # Reshape into the form needed for the heatmap
     stacked = scaled.stack() 
     stacked.index.rename(['dataset','method','measure'],inplace=True)
     # Finally, our final dataframe!
     X = pd.DataFrame(stacked).pivot_table(index=['measure','dataset'],columns=['method'])
-    print('#')
     X.to_csv('test.csv')
-    print('#')
 
     # write our dataframe
     X.to_csv('data/results/internal_measures_standardized.csv')
@@ -58,7 +52,12 @@ if __name__ == '__main__':
 
     xlabs = list(X.columns.levels[1])
 
+    ylabs = [i.upper() if i is not None else None for i in ylabs]
+    xlabs = [i.upper() if i is not None else None for i in xlabs]
+
     ax = sns.heatmap(X,0,1,cmap='viridis',xticklabels=xlabs,yticklabels=ylabs)
     ax.hlines(sep_lines, colors='r', linestyles='dotted', *ax.get_xlim())
+
+    plt.tight_layout()
     plt.show()
 
