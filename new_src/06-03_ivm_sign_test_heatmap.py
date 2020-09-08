594b94371e9c1c57ffb659054a8ad2d058d56a96
Forrest Koch <forrest.c.koch@gmail.com>
2020-09-08 12:28:40 +1000

    add final ivm pval plot

--- new_src/06-03_ivm_sign_test_heatmap.py
+++ new_src/06-03_ivm_sign_test_heatmap.py
@@ -1,10 +1,12 @@
 import sys
 import os
+import re
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
+import re
 
 plt.rcParams.update({'font.size': 6})
 
@@ -19,9 +21,9 @@ def fill_matrix(x):
 
 def sort_by_ranks(ranks,pv_matrix,measure):
     #print(ranks[ranks['measure'] == measure].values)
-    print(ranks)
-    print(pv_matrix)
-    print(measure)
+    #print(ranks)
+    #print(pv_matrix)
+    #print(measure)
     idx = np.argsort(ranks[ranks['measure'] == measure].values[0][1:])
     mlist = ranks.columns[1:][idx].tolist()
     return pd.DataFrame(pv_matrix[idx][:,idx],index=mlist,columns=mlist)
@@ -29,17 +31,17 @@ def sort_by_ranks(ranks,pv_matrix,measure):
 if __name__ == '__main__':
 
     #fig, axes = plt.subplots(3,2,figsize=(6,9))#,sharex='col',sharey='row')
-    fig, axes = plt.subplots(3,1,figsize=(6,9))#,sharex='col',sharey='row')
+    fig, axes = plt.subplots(1,3,figsize=(9,3))#,sharex='col',sharey='row')
     ranks = get_ranks()
     to_replace = {'mctsne':'tsne','nmf2':'nmf-lee','nmf':'nmf-nnsvd'}
     for measure, ax in zip(['vrc','dbs','ss-euc'],axes.flatten()):
         pvals = get_pvals(measure)
         #pval_mat = fill_matrix(pvals)
         ordered = sort_by_ranks(ranks,pvals.values,measure)
-        xlabs = [i if i not in to_replace else to_replace[i] for i in ordered.index.tolist()]
+        xlabs = [re.sub('\.','-',i) if i not in to_replace else to_replace[i] for i in ordered.index.tolist()]
         sns.heatmap(ordered,cmap='viridis',xticklabels=xlabs,yticklabels=xlabs,cbar=False,ax=ax,vmin=0,vmax=0.5)
         ax.set_title(measure)
     plt.tight_layout()
-    #plt.savefig('writeup/plots/ivm_pvals/ivm_sign_test_all.pdf')
+    plt.savefig('writeup/plots/ivm_pvals/ivm_sign_test_all-final.pdf')
     plt.show()
         
